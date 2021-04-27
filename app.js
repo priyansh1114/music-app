b726566c5e903cf287578fa34a2f3cfa24077abc
PRIYANSHU TRIPATHI <71384373+priyansh1114@users.noreply.github.com>
2021-04-27 11:16:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+window.addEventListener('load',() => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+
+// sound
+console.log(sounds)
+pads.forEach((pad,index) =>{
+    pad.addEventListener('click', function () {
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        
+    });
+});
+});
